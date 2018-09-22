ec59d8794a37be04fb3ad11ac7652c5d7cc17934
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-09-23 04:34:02 +0530

    Album view created

--- music/views.py
+++ music/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
-
+from .models import Album
 def index(request):
-    return HttpResponse('<h1>This is the music app Homepage</h1>')
+    all_albums = Album.objects.all()
+    html=''
+    for album in all_albums:
+        url = '/music/'+ str(album.id) + '/'
+        html += '<a href="'+url+'">'+album.album_title+'</a><br>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse('<h2>Details for album id : '+str(album_id)+'<h2>')
